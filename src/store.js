b24eff78b8a8093b4859ba673aeadd0f95339ce8
Joary13 <jolea13008@gmail.com>
2024-02-24 22:46:56 +0100

    reducer initialized

---
+++ src/store.js
@@ -0,0 +1,32 @@
+const initialState = {
+  balance: 0,
+  loan: 0,
+  loanPurpose: '',
+};
+
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case 'account/deposit':
+      return { ...state, balance: state.balance + action.payload };
+    case 'account/withdraw':
+      return { ...state, balance: state.balance - action.payload };
+    case 'account/requestLoan':
+      if (state.loan > 0) return state;
+      return {
+        // later
+        // balance: state.balance + action.payload.loan,
+        // loan: action.payload.loan,
+        // loanPurpose: action.payload.purpose,
+      };
+    case 'account/payLoan':
+      if (state.balance < state.loan) return;
+      return {
+        ...state,
+        balance: state.balance - state.loan,
+        loan: 0,
+        loanPurpose: '',
+      };
+    default:
+      return state;
+  }
+}
